04ae0e31c039ac7e875183d5904928a3e15121af
vahagn@!nvision.am <nvisionMicrosoft12>
2019-11-27 17:56:56 +0400

    Initial commit.

---
+++ src/common/linalg/matrix.h
@@ -0,0 +1,25 @@
+//
+// Created by vahagn on 11/27/19.
+//
+
+#ifndef STEREOVISION_MATRIX_H
+#define STEREOVISION_MATRIX_H
+
+#include "row.h"
+#include <vector>
+#include <variant>
+
+namespace StereoVision {
+
+template<typename T>
+class Matrix {
+ public:
+  Matrix(int rows, int cols){}
+ private:
+
+
+};
+
+}
+
+#endif //STEREOVISION_MATRIX_H
---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+//
+// Created by vahagn on 11/27/19.
+//
+#include "row.h"
+#include <iostream>
+#include <variant>
+#include <memory>
+
+int main(int argc, char *argv[])
+{
+  std::variant<std::shared_ptr<int []>, int *> q (std::shared_ptr<int[]>(new int[10]));
+//  q = ;
+
+  std::get<std::shared_ptr<int[]>>(q);
+
+
+
+  StereoVision::Row<int> row1(6);
+  StereoVision::Row<float> row2(6);
+
+  for (int i = 0; i < row1.Length(); ++i) {
+    row1[i] = i;
+    row2[i] = 2 * i;
+  }
+
+  std::cout << row1 << std::endl;
+  std::cout << row2 << std::endl;
+
+  auto sum = row1 + row2;
+  auto sub = row1 - row2;
+  auto mul = row1 * row2;
+
+  auto dot = row1.dot(row2);
+
+  std::cout << sum << std::endl;
+  std::cout << sub << std::endl;
+  std::cout << mul << std::endl;
+  std::cout << dot << std::endl;
+
+  return 0;
+}
